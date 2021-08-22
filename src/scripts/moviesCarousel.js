1bb33be2b0cbd9c24c7d3984e602d11bfd42d0b7
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-08-22 15:38:30 -0300

    fix-slider-bug

--- src/scripts/moviesCarousel.js
+++ src/scripts/moviesCarousel.js
@@ -1,38 +1,64 @@
-$('.movies-list').slick({
-  infinite: true,
-  slidesToShow: 7,
-  slidesToScroll: 3,
-  centerMode: true,
-  /* prevArrow: $('#arrow-prev'),
-  nextArrow: $('#arrow-next'), */
+$.when(
+  $.getJSON("https://sky-frontend.herokuapp.com/movies"),
+  $.ready
+).done(function (data) {
+  const response = data[0].contents;
+  const movies = response.filter(movie => movie.mediaType === "MOVIE");
+  const series = response.filter(movie => movie.mediaType === "SERIE");
+  console.log(movies, series);
 
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 330,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '0px',
-        slidesToShow: 2
-      }
-    }
-  ]
+  $.each(movies, function (index, item) {
+    $('.movies-list').prepend(`
+    <div class="movie">
+      <button type="button">
+        <em class="fas fa-lock"></em>
+      </button>
+      <img id="movie-cover"
+        src="${item.images[0].url}"
+        alt="${item.title}"
+        title="${item.title}"
+      />
+    </div>
+  `)
+  });
+  sliderInit();
 });
+
+function sliderInit() {
+  $('.movies-list').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    centerMode: true,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 330,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+}
